76a29f63031749232bf20db0485fc18ec22f7fc7
Gabe <gabrielc.e1123@gmail.com>
2021-06-13 23:26:28 -0400

    created test map data and implemented functionality to populate list of GameMap objects.

--- src/com/learning/GameMap.java
+++ src/com/learning/GameMap.java
@@ -3,17 +3,36 @@ package com.learning;
 import java.util.ArrayList;
 
 public class GameMap {
+    private String name;
     private int maxX;
     private int maxY;
-    private ArrayList<ArrayList<Integer>> board;
+    private int[][] board;
 
-    public GameMap(int maxX, int maxY, ArrayList<ArrayList<Integer>> board){
+    public GameMap(String name, int maxX, int maxY, int[][] board){
+        this.name = name;
         this.maxX = maxX;
         this.maxY = maxY;
         this.board = board;
     }
 
     public int getHeight(int x, int y){
-        return board.get(x).get(y);
+        return board[y][x];
+    }
+
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+
+    public void printMap(){
+        for(int y = 0; y < this.maxY; y++){
+            for(int x = 0; x < this.maxX; x++){
+                System.out.print(board[y][x] + " ");
+            }
+            System.out.print("\n");
+        }
     }
 }
--- src/com/learning/Maps.java
+++ src/com/learning/Maps.java
@@ -1,34 +1,38 @@
 package com.learning;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Maps{
-    private Map<Integer, GameMap> maps;
+    private final static Map<Integer, GameMap> maps = new HashMap<>();
 
+    //Static block to initialize GameMap objects from maps.csv file
     static {
-        try(BufferedReader scanner = new BufferedReader(new FileReader("inputs\\maps.csv"))){
+        try(BufferedReader scanner = new BufferedReader(new FileReader("src/inputs/maps.csv"))){
             String inputLine;
+            int index = 0;
             while((inputLine = scanner.readLine()) != null){
                 String[] data = inputLine.split(",");
+                int x = Integer.parseInt(data[1]), y = Integer.parseInt(data[2]);
+                int[][] tempBoard = new int[y][x];
+
+                //Populate board 2D array with tile heights
+                int current = 3;
+                for(int i = 0; i < y; i++){
+                    for(int j = 0; j < x; j++){
+                        tempBoard[i][j] = Integer.parseInt(data[current++]);
+                    }
+                }
+                maps.put(index++,new GameMap(data[0],Integer.parseInt(data[1]),Integer.parseInt(data[2]),tempBoard));
             }
         }catch (Exception e){
             e.printStackTrace();
         }
-
-
     }
 
-    public Maps(){
-        this.maps = new HashMap<>();
-    }
-
-    public GameMap getMap(int number){
+    public static GameMap getMap(int number){
         return maps.get(number);
     }
 
